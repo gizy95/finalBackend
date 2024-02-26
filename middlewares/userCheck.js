7595247ae60ba1d4af615ddac35593f2c361ea0c
Bedizz <turkyilmaz.bediz@gmail.com>
2024-02-26 11:26:38 +0100

    usercheckMD

---
+++ middlewares/userCheck.js
@@ -0,0 +1,29 @@
+import User from '../models/user.js';
+
+export const userCheck = async (req, res, next) => {
+    const { email } = req.body;
+    
+
+    if (req.method === "GET" || req.method === "DELETE" || req.method === "PUT") {
+        const user = await User.findOne({ email: email.tolowercase() });
+        if (user) {
+            req.user = user;
+            next();
+        } else {
+            return res.status(404).json({ message: "User not found" });
+            
+        }
+    }else if (req.method === "POST") {
+        const existingUser = await User.findOne({ email: email.tolowercase() });
+
+        if (existingUser) {
+            return res.status(400).json({ message: "User already exists" });
+        } else {
+            next();
+        }
+    }
+
+}
+
+        
+ 
