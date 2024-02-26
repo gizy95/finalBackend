0abcdd86f0576c14104144806bb39447dcabf3c2
Antonio Bjelobrk <bjelobrka@gmail.com>
2024-02-26 11:22:11 +0100

    added endpoint for user register

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ import 'dotenv/config';
 import express from 'express';
 import cors from "cors";
 import { connectDB } from "./db/client.js"
+import userRoutes from './routes/userRouter.js';
 
 const app = express();
 const port = 8000 || process.env.PORT;
 
 app.use(cors());
 app.use(express.json())
+app.use("/user", userRoutes)
 
 const startServer = async () => {
     await connectDB();
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,2 +1,9 @@
 import express from 'express';
+import { postUser } from '../controllers/userController.js';
 
+
+const userRoutes = express.Router();
+
+userRoutes.post("/", postUser)
+
+export default userRoutes;
