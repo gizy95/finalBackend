0b69854c090b11545ce452543220f85ce6375e25
Antonio Bjelobrk <bjelobrka@gmail.com>
2024-02-26 11:42:44 +0100

    tested the middleware and fixed small typos

--- middlewares/userCheck.js
+++ middlewares/userCheck.js
@@ -1,20 +1,20 @@
-import User from '../models/user.js';
+import User from '../models/User.js';
 
 export const userCheck = async (req, res, next) => {
     const { email } = req.body;
-    
+
 
     if (req.method === "GET" || req.method === "DELETE" || req.method === "PUT") {
-        const user = await User.findOne({ email: email.tolowercase() });
+        const user = await User.findOne({ email: email.toLowerCase() });
         if (user) {
             req.user = user;
             next();
         } else {
             return res.status(404).json({ message: "User not found" });
-            
+
         }
-    }else if (req.method === "POST") {
-        const existingUser = await User.findOne({ email: email.tolowercase() });
+    } else if (req.method === "POST") {
+        const existingUser = await User.findOne({ email: email.toLowerCase() });
 
         if (existingUser) {
             return res.status(400).json({ message: "User already exists" });
@@ -25,5 +25,4 @@ export const userCheck = async (req, res, next) => {
 
 }
 
-        
- 
+
