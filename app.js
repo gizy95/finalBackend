6ec699fa80c3ddeec3b5f772c74a14a72213162c
Antonio Bjelobrk <bjelobrka@gmail.com>
2024-02-26 14:35:45 +0100

    added the post route

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import express from 'express';
 import cors from "cors";
 import { connectDB } from "./db/client.js"
 import userRoutes from './routes/userRouter.js';
+import postRoutes from './routes/postRouter.js';
 
 const app = express();
 const port = 8000 || process.env.PORT;
@@ -10,6 +11,7 @@ const port = 8000 || process.env.PORT;
 app.use(cors());
 app.use(express.json())
 app.use("/user", userRoutes)
+app.use("/post", postRoutes)
 
 const startServer = async () => {
     await connectDB();
