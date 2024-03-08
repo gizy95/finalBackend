b17a05cf980578644fe8d1a858de9309845c4765
Antonio Bjelobrk <bjelobrka@gmail.com>
2024-03-08 17:53:45 +0100

    merged

--- routes/postRouter.js
+++ routes/postRouter.js
@@ -1,12 +1,7 @@
 import express from 'express';
-<<<<<<< HEAD
-import { postPost, getAllPosts, deletePost, postEvent, getSinglePost } from '../controllers/postController.js';
+import { postPost, getAllPosts, getFilteredAllPosts, deletePost, postEvent, getSinglePost } from '../controllers/postController.js';
 import { upload } from '../middlewares/upload.js'
-=======
-import { postPost,getAllPosts, getFilteredAllPosts, deletePost,postEvent,getSinglePost } from '../controllers/postController.js';
-import {upload} from '../middlewares/upload.js'
-import {authMiddleware} from '../middlewares/userToken.js'
->>>>>>> main
+import { authMiddleware } from '../middlewares/userToken.js'
 
 
 const postRoutes = express.Router();
@@ -14,7 +9,7 @@ const postRoutes = express.Router();
 postRoutes.post("/", upload.single('picture'), postPost) //checked
 postRoutes.get("/:id", getSinglePost) //checked
 postRoutes.get("/", getAllPosts) //checked
-postRoutes.get('/:id/all',authMiddleware,getFilteredAllPosts)
+postRoutes.get('/:id/all', authMiddleware, getFilteredAllPosts)
 postRoutes.delete('/:id', deletePost) //checked
 postRoutes.post("/event", postEvent) //checked
 postRoutes.delete('/event/:id', deletePost) //checked
