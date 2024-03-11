a2a05b37c9c1786cfe200ebf46b8d5502c5d924a
gizy95 <bjelobrka@gmail.com>
2024-03-11 11:49:10 +0100

    Update postRouter.js
    deleted some extra code

--- routes/postRouter.js
+++ routes/postRouter.js
@@ -1,13 +1,8 @@
 import express from 'express';
-<<<<<<< HEAD
-import { postPost, getAllPosts, deletePost, postEvent, getSinglePost } from '../controllers/postController.js';
-import { upload } from '../middlewares/upload.js'
-=======
+
 import { postPost,getAllPosts, getFilteredAllPosts, deletePost,postEvent,getSinglePost } from '../controllers/postController.js';
 import {upload} from '../middlewares/upload.js'
 import {authMiddleware} from '../middlewares/userToken.js'
->>>>>>> main
-
 
 const postRoutes = express.Router();
 
